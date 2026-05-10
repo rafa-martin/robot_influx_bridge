0c68fd05de91d50f210210e0526f69690602cd42
Rafael Martin <theverbat@gmail.com>
2026-05-10 12:49:54 +0200

    update license

--- robot_influx_common_interfaces/src/joint_state_translator.cpp
+++ robot_influx_common_interfaces/src/joint_state_translator.cpp
@@ -1,4 +1,31 @@
-// SPDX-License-Identifier: MIT
+// Copyright 2025, Rafael Martin. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of Rafael Martin nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 
 #include <robot_influx_bridge/translator_base.hpp>
 #include <robot_influx_bridge/translator_utils.hpp>
@@ -28,28 +55,28 @@ public:
     serializer.deserialize_message(&message, &joint_message);
 
     std::vector<std::string> lines;
-    
+
     // Create a separate line for each joint
     for (size_t i = 0; i < joint_message.name.size(); ++i) {
       std::ostringstream line;
       line << robot_influx_bridge::build_series_name(context, "joint_states");
-      
+
       // Add joint name as a tag
       line << ",joint=" << joint_message.name[i];
-      
+
       line << ' ';
-      
+
       // Add position if available
       if (i < joint_message.position.size()) {
         line << "position=" << joint_message.position[i];
       }
-      
+
       // Add velocity if available
       if (i < joint_message.velocity.size()) {
         if (i < joint_message.position.size()) line << ',';
         line << "velocity=" << joint_message.velocity[i];
       }
-      
+
       // Add effort if available
       if (i < joint_message.effort.size()) {
         if (i < joint_message.position.size() || i < joint_message.velocity.size()) line << ',';
